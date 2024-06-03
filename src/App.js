2a3476405373f5d0791dcc0d089ad59104c68774
Ogabek Jurayev <olimjurayev44@gmail.com>
2024-06-03 13:55:27 +0500

    section-6-gacha

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MySkills from "./Section-2";
 import MyExperience from "./Section-3";
 import AboutMe from "./Section-4";
 import MyProjects from "./Section-5";
-
+import MyTestimonial from "./Section-6";
 function App() {
   return (
     <div>
@@ -16,6 +16,7 @@ function App() {
       <MyExperience />
       <AboutMe />
       <MyProjects />
+      <MyTestimonial />
     </div>
   );
 }
